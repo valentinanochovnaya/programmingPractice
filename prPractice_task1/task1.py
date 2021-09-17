313f5af43568df1accf8534cf4c57cb423ace100
valentyna nochovna <1005nochovnaya@gmail.com>
2021-09-17 11:23:10 +0300

    Modify a function that finds numbers in specific range that consist of particular input numbers

--- prPractice_task1/task1.py
+++ prPractice_task1/task1.py
@@ -1,23 +1,35 @@
-def find_happy_numbers(num):
+def find_happy_numbers(num, arr):
     happy_numbers = []
+    count = 0
     temp = []
     for i in range(num):
-        string_number = str(i)
-        if len(temp) != 0:
-            happy_numbers.append(int(''.join(temp)))
-        for k in range(len(string_number)):
-            if string_number[k] == '7' or string_number[k] == '4':
-                temp.append(string_number[k])
-                continue
-            else:
-                temp = []
-                break
+       if (len(temp) != 0):
+            happy_numbers.append(int(str(''.join(temp))))
+            count = 0
+       temp = []
+       string_number = str(i)
+       for k in range(len(string_number)):
+           for n in range(len(arr)):
+               if string_number[k] == str(arr[n]):
+                   count = count + 1
+               else:
+                   continue
+       if count == len(string_number):
+            temp.append(string_number)
+       else:
+           temp = []
+           count = 0
     return happy_numbers
 
 
 try:
+    len_ = int(input("Enter a length of control array "))
+    control_array = []
+    for i in range(len_):
+        el = int(input())
+        control_array.append(el)
     number = int(input("Enter a number "))
-    result = find_happy_numbers(number)
+    result = find_happy_numbers(number, control_array)
     if len(result) == 0:
         print("Happy numbers weren't found")
     else:
